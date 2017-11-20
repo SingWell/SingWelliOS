77268456f06d3be819cbaec0d3517fad459e92a3
Tsiems <tsiems@smu.edu>
2017-11-20 14:58:53 -0600

    added notification and roster view controllers

---
+++ SingWell/SingWell/ViewControllers/RosterViewController/RosterTableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  RosterTableViewController.swift
+//  SingWell
+//
+//  Created by Travis Siems on 11/20/17.
+//  Copyright © 2017 Travis Siems. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class RosterTableViewController: UITableViewController {
+
+    var roster:[JSON] = [
+        ["name":"Jane Doe"],
+        ["name":"Susan B. Anthony"],
+        ["name":"Zachary Johnson"]
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Roster"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return roster.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
+
+        cell.textLabel?.text = roster[indexPath.row]["name"].stringValue
+        
+
+        return cell
+    }
+ 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
