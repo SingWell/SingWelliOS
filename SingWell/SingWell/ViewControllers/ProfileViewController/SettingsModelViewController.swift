2705a864d4135360dddab82c34b7d2d4bcadb2d7
Tsiems <tsiems@smu.edu>
2018-04-30 11:45:35 -0500

    Some really important changes

--- SingWell/SingWell/ViewControllers/ProfileViewController/SettingsModelViewController.swift
+++ SingWell/SingWell/ViewControllers/ProfileViewController/SettingsModelViewController.swift
@@ -14,39 +14,16 @@ class SettingsModelViewController: AnimatableModalViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     @IBAction func logoutButtonPressed(sender: Any?) {
-        //logout:
-        let alertController = UIAlertController(title: "Logout", message:
-            "Are you sure?", preferredStyle: UIAlertControllerStyle.alert)
-        alertController.addAction(UIAlertAction(title: "Yes, logout", style: UIAlertActionStyle.destructive) { action in
-            ApiHelper.logout()
-            self.performSegue(withIdentifier: "unwindToLoginSegue", sender: self)
-        })
-        alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel,handler: nil))
-        
-        self.present(alertController, animated: true, completion: nil)
+        //logout
+        ApiHelper.logout()
+        self.performSegue(withIdentifier: "unwindToLoginSegue", sender: self)
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonPressed(sender: Any?) {
+        //dismiss modal
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
 }
